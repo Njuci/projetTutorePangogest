0740007e6d90eb564727364c5474bb9bf9af3689
Njuci <augustinnjuci@gmail.com>
2024-10-09 20:04:16 +0200

    'bien Immobilier'

--- pango_app/urls.py
+++ pango_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UtilisateurViewSet,AdresseViewSet,ContratLocationViewSet
+from .views import UtilisateurViewSet,AdresseViewSet,ContratLocationViewSet,BienImmobilierViewSet
 
 # Créez un router qui gère automatiquement les routes pour le ViewSet
 router = DefaultRouter()
 router.register(r'utilisateurs', UtilisateurViewSet, basename='utilisateur')
 
 router.register(r'adresse', AdresseViewSet, basename='adresse')
+router.register(r'bien_imobilier', BienImmobilierViewSet, basename='bien_imobilier')
+
 router.register(r'contrat_location',ContratLocationViewSet,basename='contrat_location')
 #add
